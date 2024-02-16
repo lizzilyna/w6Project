1f75b5c50bdbaa436e805eb8d1d71354110db8bb
lizzlyna <barbara.panzetta@gmail.com>
2024-02-16 19:14:52 +0100

    primo

---
+++ src/main/java/it/epicode/w6Project/AppConfig.java
@@ -0,0 +1,26 @@
+package it.epicode.w6Project;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("env.properties")
+public class AppConfig {
+
+    @Bean
+    public Cloudinary getCloudinary(@Value("${cloudinary.cloud_name}") String name,
+                                    @Value("${cloudinary.api_key}") String key,
+                                    @Value("${cloudinary.api_secret}") String secret){
+
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", name,
+                "api_key", key,
+                "api_secret", secret));
+
+    }
+}
+
