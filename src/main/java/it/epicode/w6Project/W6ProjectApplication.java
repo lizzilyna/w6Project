eff812c69955dd978f9d3542132efa89f1ce0a2f
lizzlyna <barbara.panzetta@gmail.com>
2024-02-16 12:29:13 +0100

    primo

---
+++ src/main/java/it/epicode/w6Project/W6ProjectApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.w6Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W6ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W6ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/w6Project/W6ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.w6Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
