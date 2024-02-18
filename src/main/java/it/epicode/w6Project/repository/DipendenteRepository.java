87b619a5c848777c3a3b5acc2ae02a816307211e
lizzlyna <barbara.panzetta@gmail.com>
2024-02-18 19:35:27 +0100

    tentativo di assegnazione andata buca

--- src/main/java/it/epicode/w6Project/repository/DipendenteRepository.java
+++ src/main/java/it/epicode/w6Project/repository/DipendenteRepository.java
@@ -3,7 +3,9 @@ package it.epicode.w6Project.repository;
 import it.epicode.w6Project.model.Dipendente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DipendenteRepository extends JpaRepository<Dipendente, Integer>, PagingAndSortingRepository<Dipendente, Integer> {
 }
 
--- src/main/java/it/epicode/w6Project/repository/DispositivoRepository.java
+++ src/main/java/it/epicode/w6Project/repository/DispositivoRepository.java
@@ -3,6 +3,8 @@ package it.epicode.w6Project.repository;
 import it.epicode.w6Project.model.Dispositivo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DispositivoRepository extends JpaRepository<Dispositivo, Integer>, PagingAndSortingRepository<Dispositivo, Integer> {
 }
